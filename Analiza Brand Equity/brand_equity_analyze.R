f8d4e3c48b7ee22018ffad41c233f1da5fffdc8f
Adrian Stodolski <adrianstodolski@protonmail.com>
2022-01-30 15:33:34 +0100

    Fixes

--- Analiza Brand Equity/brand_equity_analyze.R
+++ Analiza Brand Equity/brand_equity_analyze.R
@@ -23,7 +23,7 @@ df1 <- read.csv2("~/Code/Data_Science_Professional_Project/Analiza Brand Equity/
 
 # Select only important data.
 df1 <- select(df1, RecordNo, T6M1:T7M10)
-
+head(df1)
 # Split data T6 and T7 questions into one column.
 df1 <- pivot_longer(df1,
                     cols = matches("T[67]M")
@@ -42,7 +42,7 @@ df1 <- pivot_wider(df1
 head(df1)
 # Create RecordNo dataset, will be useful to later join data for all calculations.
 RecordNo <- df1 %>% select(RecordNo) %>% distinct() %>% mutate(project_id=1)
-
+head(RecordNo)
 # Create plants names dataset, will be useful to later join data for all calculations.
 plants <- bind_rows(df1 %>% select(plants=T6), df1 %>% select(plants=T7)) %>%
   filter(!is.na(plants)) %>% distinct() %>% mutate(project_id=1)
@@ -74,10 +74,10 @@ df2 <- read.csv2("~/Code/Data_Science_Professional_Project/Analiza Brand Equity/
 
 # Select only important data.
 df2 <- select(df2, RecordNo, T9M1:T10M10)
-
+head(df2)
 # Split data T9 and T10 questions into one column.
 df2 <- pivot_longer(df2,
-                    cols = matches("T[910]M")
+                    cols = matches("T[9]M, T[10]M")
                     ,names_to = c("T","index")
                     ,names_pattern = "(T9|T10)M(\\d+)"
 )
@@ -87,13 +87,13 @@ df2 <- mutate(df2, index=as.numeric(index))
 head(df2)
 # Separate T9 and T10 questions.
 df2 <- pivot_wider(df2
-                   ,names_from = all_of(T)
+                   ,names_from = T
                    ,values_from = value
 )
-
+head(df2)
 # Join plants with RecordNo data.
 data_together2 <- RecordNo %>% left_join(plants)
-
+head(data_together2)
 # Create Usage score
 
 # For T9 question
