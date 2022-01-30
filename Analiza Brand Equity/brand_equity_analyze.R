afe41076eb73054ad1de78f4341302b0cf168dff
Adrian Stodolski <adrianstodolski@protonmail.com>
2022-01-30 17:36:25 +0100

    Fixes

--- Analiza Brand Equity/brand_equity_analyze.R
+++ Analiza Brand Equity/brand_equity_analyze.R
@@ -220,7 +220,7 @@ summary(Total$plants)
 
 # Add plants labels
 MyLabels <- source("~/Code/Data_Science_Professional_Project/Analiza Brand Equity/labels.R", encoding = "UTF8")
-Total$mushrooms <- MyLabels(Total$plants)
+Total$plants <- MyLabels(Total$plants)
 
 # Write data to outfile.
 write.csv2(Total, "Brand_Equity_Results.csv", row.names = FALSE)
