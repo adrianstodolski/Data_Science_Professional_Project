9767334e9a3c157a122645b8b18074ec07d91f0d
Adrian Stodolski <adrianstodolski@protonmail.com>
2022-01-30 18:04:25 +0100

    Fix

--- Analiza Brand Equity/brand_equity_analyze.R
+++ Analiza Brand Equity/brand_equity_analyze.R
@@ -15,7 +15,7 @@ install.packages("tidyverse", dependencies = TRUE)
 library(tidyverse)
 
 
-# -------------------------------- AWARNESS ------------------------------------
+# -------------------------------- AWARNESS -----------------------------------#
 # According to Brand Health Index description, awarness questions are T6 and T7.
 
 # Load file.
@@ -23,33 +23,33 @@ df1 <- read.csv2("~/Code/Data_Science_Professional_Project/Analiza Brand Equity/
 
 # Select only important data.
 df1 <- select(df1, RecordNo, T6M1:T7M10)
-head(df1)
+
 # Split data T6 and T7 questions into one column.
 df1 <- pivot_longer(df1,
                     cols = matches("T[67]M")
                     ,names_to = c("T","index")
                     ,names_pattern = "(T6|T7)M(\\d+)"
 )
-head(df1)
+
 # Transform index as numeric 
 df1 <- mutate(df1, index=as.numeric(index))
-head(df1)
+
 # Separate T6 and T7 questions.
 df1 <- pivot_wider(df1
                    ,names_from = T
                    ,values_from = value
 )
-head(df1)
+
 # Create RecordNo dataset, will be useful to later join data for all calculations.
 RecordNo <- df1 %>% select(RecordNo) %>% distinct() %>% mutate(project_id=1)
-head(RecordNo)
+
 # Create plants names dataset, will be useful to later join data for all calculations.
 plants <- bind_rows(df1 %>% select(plants=T6), df1 %>% select(plants=T7)) %>%
   filter(!is.na(plants)) %>% distinct() %>% mutate(project_id=1)
-head(plants)
+
 # Join plants with RecordNo data.
 data_together <- RecordNo %>% left_join(plants)
-head(data_together)
+
 
 # Create Awareness score 
 
@@ -66,34 +66,34 @@ data_together <- mutate(data_together, Awareness_score=if_else(!is.na(index),2,A
 data_together <- select(data_together, -index)
 
 
-# -------------------------------- USAGE ---------------------------------------
+# -------------------------------- USAGE --------------------------------------#
 # According to Brand Health Index description, usage questions are T9 and T4.
 
 # Load file.
 df2 <- read.csv2("~/Code/Data_Science_Professional_Project/Analiza Brand Equity/data.csv")
-head(df2)
+
 # Select only important data.
 df2 <- select(df2, RecordNo, T9M1:T4M10)
-head(df2)
-# Split data T9 and T10 questions into one column.
+
+# Split data T9 and T4 questions into one column.
 df2 <- pivot_longer(df2,
                     cols = matches("T[94]M")
                     ,names_to = c("T","index")
                     ,names_pattern = "(T9|T4)M(\\d+)"
 )
-head(df2)
+
 # Transform index as numeric 
 df2 <- mutate(df2, index=as.numeric(index))
-head(df2)
-# Separate T9 and T10 questions.
+
+# Separate T9 and T4 questions.
 df2 <- pivot_wider(df2
                    ,names_from = T
                    ,values_from = value
 )
-head(df2)
+
 # Join plants with RecordNo data.
 data_together2 <- RecordNo %>% left_join(plants)
-head(data_together2)
+
 # Create Usage score
 
 # For T9 question
@@ -103,13 +103,13 @@ data_together2 <- mutate(data_together2, Usage_score=if_else(index==1,5,Usage_sc
 data_together2 <- mutate(data_together2, Usage_score=if_else(index>=2,4,Usage_score,missing = Usage_score))
 data_together2 <- select(data_together2, -index)
 
-# For T10 question
+# For T4 question
 data_together2 <- data_together2 %>% left_join(df2 %>% select(-T9) %>% rename(plants=T4))
 data_together2 <- mutate(data_together2, Usage_score=if_else(!is.na(index),2,Usage_score,missing = Usage_score))
 data_together2 <- select(data_together2, -index)
 
 
-### ------------------------------ PREFERENCE ----------------------------------
+### ------------------------------ PREFERENCE ---------------------------------#
 # According to Brand Health Index description, preference question is T13.
 
 # Load file.
@@ -174,7 +174,7 @@ df5 <- read.csv2("~/Code/Data_Science_Professional_Project/Analiza Brand Equity/
   mutate(FutureUsage_score=if_else(FutureUsage_score==999,1,FutureUsage_score, missing =FutureUsage_score))
 
 
-# -------------------------------- SATISFACTION --------------------------------
+# -------------------------------- SATISFACTION -------------------------------#
 # According to Brand Health Index description, Future use question is T12.
 
 # Satisfaction score
@@ -188,7 +188,7 @@ df6 <- read.csv2("~/Code/Data_Science_Professional_Project/Analiza Brand Equity/
   mutate(Satisfaction_score=if_else(Satisfaction_score==999,1,Satisfaction_score, missing =Satisfaction_score))
 
 
-# -------------------------------- TOTAL ---------------------------------------
+# -------------------------------- TOTAL --------------------------------------#
 # This is final calculations. Here there is joining all above analysis results and
 # eliminate outliers.
 
